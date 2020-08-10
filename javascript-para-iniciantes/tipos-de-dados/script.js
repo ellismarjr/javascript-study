99564ed86cbed32e3c75c26bdba6c00286e811d7
Júnior de Oliveira <ellismarjr@hotmail.com>
2020-08-09 21:17:01 -0300

    first javascript content

---
+++ javascript-para-iniciantes/tipos-de-dados/script.js
@@ -0,0 +1,4 @@
+var nome = 'Junior';
+var idade = 28;
+var time = null;
+console.log(typeof time);
---
+++ javascript-para-iniciantes/variaveis/script.js
@@ -0,0 +1,8 @@
+var nome = 'Junior';
+var idade = 30;
+var comidaFavorita;
+var possuiFaculdade = true;
+
+comidaFavorita = 'Pizza';
+
+console.log(nome, idade, comidaFavorita);
